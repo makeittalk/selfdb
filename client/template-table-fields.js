d0a605e37cb95b949a7d569ebd0448591a713957
Ovidio Pozo <ovidio.pozo@jalasoft.com>
2014-10-10 17:30:40 -0400

    List of fields on a table 	modified:   .meteor/release 	modified:   .meteor/versions 	modified:   client/table-fields.html 	modified:   client/template-table-fields.js

--- client/template-table-fields.js
+++ client/template-table-fields.js
@@ -1,3 +1,24 @@
 Template.tablefields.fields = function () {
-    return Fields.find({}, {sort: {name: 1}}).fetch();
+     var fieldList = Fields.find({}, {sort: {name: 1}}).fetch();
+     console.log(fieldList);
+
+     return _.map(fieldList,
+     	convertToArray
+     	);
+; 
+
 };
+
+function convertToArray(object){
+	var res=[];
+	_.each(Object.getOwnPropertyNames(object),
+     	function(val){
+     		var ob = {};
+     		ob["name"]=val;
+     		ob["value"]=object[val];
+     		res.push(ob);
+     	});
+	var resp = {};
+	resp["fieldDescription"] = res;
+	return resp;
+}
